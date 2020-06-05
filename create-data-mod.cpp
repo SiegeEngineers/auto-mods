d23844f643fece154ff53688d1bb7de64579c761
HSZemi <hszemi@users.noreply.github.com>
2020-06-05 21:38:52 +0200

    Remove ram-fix
    The ram crash has been fixed in Hotfix 37906 for DE

--- create-data-mod.cpp
+++ create-data-mod.cpp
@@ -7,15 +7,11 @@ using namespace std;
 
 int main(int argc, char **argv) {
     const string FLYING_DUTCHMAN = "flying-dutchman";
-    const string FLYING_DUTCHMAN_WITH_RAM_FIX = "flying-dutchman-with-ram-fix";
-    const string RAM_FIX = "ram-fix";
 
     if (argc < 4) {
         cout << "Usage: " << argv[0] << " <mod-identifier> source.dat target.dat" << endl;
         cout << "Where <mod-identifier> is one of the following:" << endl;
         cout << "    " << FLYING_DUTCHMAN << endl;
-        cout << "    " << FLYING_DUTCHMAN_WITH_RAM_FIX << endl;
-        cout << "    " << RAM_FIX << endl;
         return 1;
     }
 
@@ -28,11 +24,6 @@ int main(int argc, char **argv) {
     char *modIdentifier = argv[1];
     if (FLYING_DUTCHMAN == modIdentifier) {
         makeTransportShipsFly(df);
-    } else if (FLYING_DUTCHMAN_WITH_RAM_FIX == modIdentifier) {
-        makeTransportShipsFly(df);
-        preventRamsAndSiegeTowersFromBoardingTransportShips(df);
-    } else if (RAM_FIX == modIdentifier) {
-        preventRamsAndSiegeTowersFromBoardingTransportShips(df);
     } else {
         cout << "Unknown mod identifier: '" << modIdentifier << "'" << endl;
     }
