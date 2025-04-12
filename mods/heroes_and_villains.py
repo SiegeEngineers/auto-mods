cf9f3a145ceb0f29d89f757d60d90f2c1a823e18
Tristan Wise <tristanwise1995@gmail.com>
2025-04-11 23:09:41 -0400

    push working trainable heroes mod

--- mods/heroes_and_villains.py
+++ mods/heroes_and_villains.py
@@ -3,10 +3,9 @@ import logging
 from genieutils.civ import Civ
 from genieutils.unit import Unit
 from genieutils.effect import Effect, EffectCommand
-from genieutils.tech import Tech
+from genieutils.tech import Tech, ResearchResourceCost
 from genieutils.datfile import DatFile
 from genieutils.unit import ResourceCost, ResourceStorage
-from genieutils.task import Task
 from mods.util import clone
 from mods.ids import RICHARD_THE_LIONHEART, TSAR_KONSTANTIN, BELISARIUS, WILLIAM_WALLACE, \
     WANG_TONG, THEODORIC_THE_GOTH, KUSHLUK, SHAH_ISHMAIL, SALADIN, SELIM_THE_GRIM, JARL, \
@@ -17,7 +16,7 @@ from mods.ids import RICHARD_THE_LIONHEART, TSAR_KONSTANTIN, BELISARIUS, WILLIAM
     THOROS, JOAN_OF_ARC, MINAMOTO, ULRICH_VON_JUNGINGEN, PACHACUTI, RAJENDRA_CHOLA, POPE_LEO_I, \
     TYPE_POPULATION_HEADROOM, TYPE_CURRENT_POPULATION, TYPE_TOTAL_UNITS_OWNED, TYPE_FOOD_STORAGE, \
     TYPE_GOLD_STORAGE, TYPE_CASTLE_TRAIN_LOCATION, TYPE_CURRENT_AGE, EFFECT_THIRISIDAI_MAKE_AVIALABLE, \
-    TYPE_ENABLE_DISABLE_UNIT
+    TYPE_ENABLE_DISABLE_UNIT, TECH_REQUIREMENT_IMPERIAL_AGE, TECH_CASTLE_BUILT
 
 NAME = 'heroes-and-villains'
 
@@ -74,14 +73,80 @@ HERO_FOR_CIV = {
 }
 
 
-def makeUnitTrainableInCastle(unit: Unit, civ: Civ):
-    #make unit cost money
+def makeUnitTrainableInCastle(unit: Unit, civ: Civ, data: DatFile):
+
+
+    print('----------------------------------------')
+    print('OLD UNIT', civ.units[25])
+    print('----------------------------------------')
+    print('NEW UNIT', unit)
+    print('----------------------------------------')
+    return unit
+
+def addUnitToCiv(civ_id: int, unit_id: int, data: DatFile):
+    logging.info(f'Making effect for hero unit for {data.civs[civ_id].units[unit_id].name} - {unit_id} for civ {data.civs[civ_id].name} - {civ_id}')
+    effect_command = EffectCommand(
+        type=TYPE_ENABLE_DISABLE_UNIT,
+        a=unit_id,
+        b=1,
+        c=-1,
+        d=0.0,
+    )
+    effect = Effect(
+        name=f'Create Hero Unit for {data.civs[civ_id].units[unit_id].name}',
+        effect_commands=[effect_command]
+    )
+    effect_id = len(data.effects)
+    data.effects.append(effect)
+    print('effect', data.effects[effect_id])
+    print('teutonic effect', data.effects[273])
+
+
+    logging.info(f'Making tech for hero unit {data.civs[civ_id].units[unit_id].name} for civ {data.civs[civ_id].name} for effect {effect_id}')
+    tech = Tech(
+        required_techs=(TECH_REQUIREMENT_IMPERIAL_AGE, TECH_CASTLE_BUILT, -1, -1, -1, -1), #imperial age
+        resource_costs=(
+            ResearchResourceCost(type=-1, amount=0, flag=0),
+            ResearchResourceCost(type=-1, amount=0, flag=0),
+            ResearchResourceCost(type=-1, amount=0, flag=0)
+        ),
+        required_tech_count=2,
+        civ=civ_id,
+        full_tech_mode=1,
+        research_location=-1,
+        language_dll_name=0,
+        language_dll_description=0,
+        research_time=0,
+        effect_id=effect_id,
+        type=0,
+        icon_id=-1,
+        button_id=0,
+        language_dll_help=0,
+        language_dll_tech_tree=0,
+        hot_key=-1,
+        name=f'Make Hero Unit Available for {data.civs[civ_id].units[unit_id].name}',
+        repeatable=1,
+    )
+    data.techs.append(tech)
+    print('tech', tech)
+    print('teutonic tech', data.techs[276])
+
+def makeHero(unitData: dict, civ: Civ, data: DatFile) -> int:
+    #prevent_hp_increase(cloned_unit)
+    logging.info(f'Patching hero unit {unitData["unitId"]}')
+    new_unit_id = len(civ.units)
+    #clone the unit
+    unit = clone(civ.units[unitData["unitId"]], data.version)
+    #set id to be end of civ units
+    unit.id = new_unit_id
+
+    #TODO balance out the units stats compared to new DLC
+    unit.hit_points = 500
+    #make unit cost resources
     unit.creatable.resource_costs = (
         ResourceCost(type=TYPE_FOOD_STORAGE, amount=500, flag=1),
         ResourceCost(type=TYPE_GOLD_STORAGE, amount=500, flag=1),
-        #require imperial age
         unit.creatable.resource_costs[2]
-        #ResourceCost(type=TYPE_CURRENT_AGE, amount=3, flag=0),
     )
     #make sure unit take up population space
     unit.resource_storages = (
@@ -90,84 +155,29 @@ def makeUnitTrainableInCastle(unit: Unit, civ: Civ):
         ResourceStorage(type=TYPE_TOTAL_UNITS_OWNED, amount=1, flag=1),
     )
     #make unit trainable in the castle
-    #print(unit)
-    unit.train_location_id = TYPE_CASTLE_TRAIN_LOCATION
-    unit.train_button = 4
-    unit.hero_mode = 1
-    
-    #TODO make unit traininable only once if the unit is alive
+    unit.creatable.train_location_id = TYPE_CASTLE_TRAIN_LOCATION
+    unit.creatable.button_id = 4
+    unit.creatable.hero_mode = 1
 
-    return unit
-
-def addUnitToCiv(civ_id: int, unitId: int, data: DatFile):
-    logging.info(f'Making effect for hero unit {data.civs[civ_id].units[unitId].name} for civ {data.civs[civ_id].name}')
-    effect_command = EffectCommand(
-        type=TYPE_ENABLE_DISABLE_UNIT,
-        a=2,
-        b=unitId,
-        c=-1,
-        d=0,
-    )
-
-    effect = Effect(
-        name=f'Create Hero Unit {data.civs[civ_id].units[unitId].name}',
-        effect_commands=[effect_command]
-    )
-    effect_id = len(data.effects)
-    data.effects.append(effect)
-    
-    logging.info(f'Making tech for hero unit {data.civs[civ_id].units[unitId].name} for civ {data.civs[civ_id].name} for effect {effect_id}')
-    #TODO create tech that activates "make available" that was just made require 103 imperial age
-    tech = Tech(
-        required_techs=(103, -1, -1, -1, -1, -1), #imperial age
-        resource_costs=(),
-        required_tech_count=1,
-        civ=civ_id,
-        full_tech_mode=1,
-        research_location=-1,
-        language_dll_name=0,
-        language_dll_description=0,
-        research_time=5,
-        effect_id=effect_id,
-        type=-1,
-        icon_id=103,
-        button_id=0,
-        language_dll_help=0,
-        language_dll_tech_tree=0,
-        hot_key=0,
-        name=f'Make Hero Unit Available {data.civs[civ_id].units[unitId].name}',
-        repeatable=1,
-    )
-    data.techs.append(tech)
-
-def makeHero(unitData: dict, civ: Civ, data: DatFile) -> int:
-    #prevent_hp_increase(cloned_unit)
-    logging.info(f'Patching hero unit {unitData["unitId"]}')
-    clone_unit_id = len(civ.units)
-    #clone the unit
-    cloned_unit = clone(civ.units[unitData["unitId"]], data.version)
-    #set id to be end of civ units
-    cloned_unit.id = clone_unit_id
-
-
-    #TODO balance out the units stats compared to new DLC
-    cloned_unit.hit_points = 300
-
-    #TODO make the unit buildable in imperial age at the castle
-    trainable_unit = makeUnitTrainableInCastle(cloned_unit, civ)
+    #TODO make unit trainable only one at a time with resources, kinda like praetorian
 
     #add the new unit to the civ
-    civ.units.append(trainable_unit)
-
-    logging.info(f'Patched hero unit {cloned_unit.name} for civ {civ.name}')
-    return clone_unit_id
+    civ.units.append(unit)
+    logging.info(f'Patched hero unit {unit.name} for civ {civ.name}')
+    return new_unit_id
 
 
 def mod(data: DatFile):
     for civ_id, civ in enumerate(data.civs):
         if civ.name not in IGNORED_CIVS and HERO_FOR_CIV[civ.name]["unitId"] is not None:
             logging.info(f'Creating hero for civ {civ.name}')
-            new_unit_id = makeHero(HERO_FOR_CIV[civ.name], civ, data)
+            hero = HERO_FOR_CIV[civ.name]
+            new_unit_id = makeHero(hero, civ, data)
             addUnitToCiv(civ_id, new_unit_id, data)
+            logging.info(f'Creating hero for civ other civs')
+#            for rest_of_civs_civ_id, rest_of_civs_civ in enumerate(data.civs):
+#                if rest_of_civs_civ_id != civ_id:
+#                    #make the hero for the other civs
+#                    makeHero(hero, rest_of_civs_civ, data)
         else:
             logging.info(f'Failed to find hero for civ {civ.name}')
