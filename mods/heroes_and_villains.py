6f936809f9ff3829c72285142009445a083d3ded
Tristan Wise <tristanwise1995@gmail.com>
2025-04-14 21:50:41 -0400

    update heroes, dont override 501 for sparta, update ids, allow water and land overlap for vikings

--- mods/heroes_and_villains.py
+++ mods/heroes_and_villains.py
@@ -7,80 +7,83 @@ from genieutils.tech import Tech, ResearchResourceCost
 from genieutils.datfile import DatFile
 from genieutils.unit import ResourceCost, ResourceStorage
 from mods.util import clone
-from mods.ids import RICHARD_THE_LIONHEART, TSAR_KONSTANTIN, BELISARIUS, WILLIAM_WALLACE, \
-    WANG_TONG, THEODORIC_THE_GOTH, KUSHLUK, SHAH_ISHMAIL, SALADIN, SELIM_THE_GRIM, JARL, \
-    ITZCOATL, ATTILA_THE_HUN, PACAL_II, EL_CID_CAMPEADOR, GENGHIS_KHAN, FRANCESCO_SFORZA, \
+from mods.ids import TABINSHWEHTI, TSAR_KONSTANTIN, BELISARIUS, WILLIAM_WALLACE, \
+    WANG_TONG, ALARIC_THE_GOTH, SUNDJATA, SHAH_ISHMAIL, SALADIN, SELIM_THE_GRIM, JARL, \
+    CUAUHTEMOC, ATTILA_THE_HUN, PACAL_II, EL_CID_CAMPEADOR, GENGHIS_KHAN, FRANCESCO_SFORZA, \
     MIKLOS_TOLDI, ALEXANDER_NEVSKI, TARIQ_IBN_ZIYAD, DAGNAJAN, SUMANGURU, BAYINNAUNG, \
-    SURYAVARMAN_I, GAJAH_MADA, DINH_LE, KOTYAN_KHAN, VYTAUTAS_THE_GREAT, QUTLUGH, OSMAN, \
+    SURYAVARMAN_I, GAJAH_MADA, LE_LOI, KOTYAN_KHAN, ALGIRDAS, QUTLUGH, OSMAN, \
     JOHN_THE_FEARLESS, ROGER_BOSSO, JAN_ZIZKA, JADWIGA, IBRAHIM_LODI, PRITHVIRAJ, TAMAR, \
-    THOROS, JOAN_OF_ARC, MINAMOTO, ULRICH_VON_JUNGINGEN, PACHACUTI, RAJENDRA_CHOLA, POPE_LEO_I, \
+    THOROS, JOAN_OF_ARC, NOBUNAGA, ULRICH_VON_JUNGINGEN, PACHACUTI, RAJENDRA_CHOLA, POPE_LEO_I, \
     VASCO_DA_GAMA, ADMIRAL_YI_SHUN_SHIN, MIHIRA_BHOJA, LEIF_ERIKSON, EDWARD_LONGSHANKS, \
+    THEMISTOCLES, LEONIDAS, DARIUS, \
     TYPE_POPULATION_HEADROOM, TYPE_CURRENT_POPULATION, TYPE_TOTAL_UNITS_OWNED, TYPE_FOOD_STORAGE, \
-    TYPE_GOLD_STORAGE, TYPE_CASTLE_TRAIN_LOCATION, TYPE_DOCK_TRAIN_LOCATION, PETARD_DEATH_ANIMATION, \
-    TYPE_ENABLE_DISABLE_UNIT, TECH_REQUIREMENT_IMPERIAL_AGE, TYPE_INFLUENCE_ABILITY, \
-    TYPE_SPAWN_CAP, TYPE_UNIT_LIMIT_RESOURCE, TYPE_POPULATION_HEADROOM, TYPE_TOTAL_UNITS_OWNED, \
+    TYPE_GOLD_STORAGE, TYPE_CASTLE_TRAIN_LOCATION, TYPE_DOCK_TRAIN_LOCATION, TYPE_POPULATION_HEADROOM, \
+    TYPE_ENABLE_DISABLE_UNIT, TECH_REQUIREMENT_IMPERIAL_AGE, TYPE_INFLUENCE_ABILITY,  TYPE_TOTAL_UNITS_OWNED,\
     TYPE_SPAWN_UNIT, TOWN_CENTER, TYPE_TOWN_CENTER_BUILT, SPECIAL_UNIT_SPAWN_BASILIEUS_DEAD, \
-    WARSHIP_CLASS, CAVLARY_CLASS, INFANTRY_CLASS, ARCHER_CLASS, CAVALRY_ARCHER_CLASS, MONK_CLASS, HAND_CANNONEER_CLASS \
+    WARSHIP_CLASS, CAVLARY_CLASS, INFANTRY_CLASS, ARCHER_CLASS, CAVALRY_ARCHER_CLASS, MONK_CLASS, HAND_CANNONEER_CLASS, \
+    WAR_ELEPHANT_CLASS, ELEPHANT_ARCHER_CLASS, CONQUISTADOR_CLASS, HEALER_CLASS \
 
 from mods.auras import attackSpeed, movementSpeed, healing
 
+#reserve spaces for hidden resouces. Dont use 501 as its used for sparta already.
+LAND_BASILIUS_RESOURCE_VALUE = 502
+WATER_BASILIUS_RESOURCE_VALUE = 503
+
 NAME = 'heroes-and-villains'
 
-#NOTE DO NOT REMOVE SPARTANS, WE USE THEIR POLEMARCH MECHANIC AND GIVING THEM EXTRA HEROES WOULD BREAK SHIT.
-IGNORED_CIVS = ['Gaia', 'Achaemenids', 'Spartans', 'Athenians', 'Shu', 'Wei', 'Wu', "Khitans", 'Jurchens']
-
 #TODO Maybe allow for multiple heroes, or randomize hero on each build.
-#TODO Maybe generate water hero(es) alongside land hero(es)
+CIVS_WITH_HEROES_ALREADY = ['Shu', 'Wu', 'Wei']
 HERO_FOR_CIV = {
-    "British": { "unitId": EDWARD_LONGSHANKS, "unitStatChanges": {} },
-    "Byzantine": { "unitId": BELISARIUS, "unitStatChanges": {} },
-    "Celts": { "unitId": WILLIAM_WALLACE, "unitStatChanges": {} },
-    "Chinese": { "unitId": WANG_TONG, "unitStatChanges": {} },
-    "French": { "unitId": JOAN_OF_ARC, "unitStatChanges": {} }, #BERNARD_DE_ARMAGNAC is cool too
-    "Goths": { "unitId": THEODORIC_THE_GOTH, "unitStatChanges": {} },
-    "Japanese": { "unitId": MINAMOTO, "unitStatChanges": {} },
-    "Mongols": { "unitId": KUSHLUK, "unitStatChanges": {} },
-    #GENGHIS_KHAN is another option but not unique skin
-    "Persians": { "unitId": SHAH_ISHMAIL, "unitStatChanges": {} },
-    "Saracens": { "unitId": SALADIN, "unitStatChanges": {} },
-    "Teutons": { "unitId": ULRICH_VON_JUNGINGEN, "unitStatChanges": {} },
-    "Turks": { "unitId": OSMAN, "unitStatChanges": {} },
-    "Vikings": { "unitId": LEIF_ERIKSON, "unitStatChanges": {} },
-    "Aztecs": { "unitId": ITZCOATL, "unitStatChanges": {} },
-    "Huns": { "unitId": ATTILA_THE_HUN, "unitStatChanges": {} },
-    "Koreans": { "unitId": ADMIRAL_YI_SHUN_SHIN, "unitStatChanges": {} },
-    "Mayan": { "unitId": PACAL_II, "unitStatChanges": {} },
-    "Spanish": { "unitId": EL_CID_CAMPEADOR, "unitStatChanges": {} },
-    "Incas": { "unitId": PACHACUTI, "unitStatChanges": {} },
-     #"Indians": { "unitId": "unit_indians", "unitStatChanges": {} },
-    "Italians": { "unitId": FRANCESCO_SFORZA, "unitStatChanges": {} },
-    "Magyars": { "unitId": MIKLOS_TOLDI, "unitStatChanges": {} },
-    "Slavs": { "unitId": ALEXANDER_NEVSKI, "unitStatChanges": {} },
-    "Berbers": { "unitId": TARIQ_IBN_ZIYAD, "unitStatChanges": {} },
-    "Ethiopians": { "unitId": DAGNAJAN, "unitStatChanges": {} },
-    "Malians": { "unitId": SUMANGURU, "unitStatChanges": {} },
-    "Portuguese": { "unitId": VASCO_DA_GAMA, "unitStatChanges": {}},
-    "Burmese": { "unitId": BAYINNAUNG, "unitStatChanges": {} },
-    "Khmer": { "unitId": SURYAVARMAN_I, "unitStatChanges": {} },
-    "Malay": { "unitId": GAJAH_MADA, "unitStatChanges": {} },
-    "Vietnamese": { "unitId": DINH_LE, "unitStatChanges": {} },
-    "Bulgarians": { "unitId": TSAR_KONSTANTIN, "unitStatChanges": {} },
-    "Cumans": { "unitId": KOTYAN_KHAN, "unitStatChanges": {} },
-    "Lithuanians": { "unitId": VYTAUTAS_THE_GREAT, "unitStatChanges": {} },
-    "Tatars": { "unitId": QUTLUGH, "unitStatChanges": {} },
-    "Burgundians": { "unitId": JOHN_THE_FEARLESS, "unitStatChanges": {} },
-    "Sicilians": { "unitId": ROGER_BOSSO, "unitStatChanges": {} },
-    "Bohemians": { "unitId": JAN_ZIZKA, "unitStatChanges": {} },
-    "Poles": { "unitId": JADWIGA, "unitStatChanges": {} },
-    "Hindustanis": { "unitId": IBRAHIM_LODI, "unitStatChanges": {} },
-    #MIHIRA_BHOJA is kinda between bengali / gurjaras and more gurjaras depending on source. Would change if other option
-    "Bengalis": { "unitId": MIHIRA_BHOJA, "unitStatChanges": {} }, 
-    "Gurjaras": { "unitId": PRITHVIRAJ, "unitStatChanges": {} },
-    "Dravidians": { "unitId": RAJENDRA_CHOLA, "unitStatChanges": {} },
-    "Romans": { "unitId": POPE_LEO_I, "unitStatChanges": {} },
-    "Armenians": { "unitId": THOROS, "unitStatChanges": {} },
-    "Georgians": { "unitId": TAMAR, "unitStatChanges": {} }
+    "British": [EDWARD_LONGSHANKS],
+    "Byzantine": [BELISARIUS],
+    "Celts": [WILLIAM_WALLACE],
+    "Chinese": [WANG_TONG],
+    "French": [JOAN_OF_ARC],
+    "Goths": [ALARIC_THE_GOTH],
+    "Japanese": [NOBUNAGA],
+    "Mongols": [GENGHIS_KHAN],
+    "Persians": [SHAH_ISHMAIL],
+    "Saracens": [SALADIN],
+    "Teutons": [ULRICH_VON_JUNGINGEN],
+    "Turks": [OSMAN],
+    "Vikings": [JARL, LEIF_ERIKSON],
+    "Aztecs": [CUAUHTEMOC],
+    "Huns": [ATTILA_THE_HUN],
+    "Koreans": [ADMIRAL_YI_SHUN_SHIN],
+    "Mayan": [PACAL_II],
+    "Spanish": [EL_CID_CAMPEADOR],
+    "Incas": [PACHACUTI],
+    "Italians": [FRANCESCO_SFORZA],
+    "Magyars": [MIKLOS_TOLDI],
+    "Slavs": [ALEXANDER_NEVSKI],
+    "Berbers": [TARIQ_IBN_ZIYAD],
+    "Ethiopians": [DAGNAJAN],
+    "Malians": [SUNDJATA],
+    "Portuguese": [VASCO_DA_GAMA],
+    "Burmese": [TABINSHWEHTI],
+    "Khmer": [SURYAVARMAN_I],
+    "Malay": [GAJAH_MADA],
+    "Vietnamese": [LE_LOI],
+    "Bulgarians": [TSAR_KONSTANTIN],
+    "Cumans": [KOTYAN_KHAN],
+    "Lithuanians": [ALGIRDAS],
+    "Tatars": [QUTLUGH], #Tamerlane 
+    "Burgundians": [JOHN_THE_FEARLESS],
+    "Sicilians": [ROGER_BOSSO],
+    "Bohemians": [JAN_ZIZKA],
+    "Poles": [JADWIGA], #Jogaila
+    "Hindustanis": [IBRAHIM_LODI],
+    "Bengalis": [MIHIRA_BHOJA], #maybe not good
+    "Gurjaras": [PRITHVIRAJ],
+    "Dravidians": [RAJENDRA_CHOLA],
+    "Romans": [POPE_LEO_I],
+    "Armenians": [THOROS],
+    "Georgians": [TAMAR],
+    "Spartans": [LEONIDAS],
+    "Achaemenids": [DARIUS],
+    "Athenians": [THEMISTOCLES]
 }
+
 
 def addUnitToCiv(civ_id: int, unit_id: int, data: DatFile):
     #create unit
@@ -130,32 +133,35 @@ def addUnitToCiv(civ_id: int, unit_id: int, data: DatFile):
     data.techs.append(enable_hero_tech)
 
 
-def limitCivToOneHero(civ_id: int, unit_id: int, data: DatFile):
-    logging.info(f'Making effect for hero unit limit for {data.civs[civ_id].units[unit_id].name} - {unit_id} for civ {data.civs[civ_id].name} - {civ_id}')
-    limit_unit_creatable_effect_command = EffectCommand(
-        type=1,
-        a=TYPE_SPAWN_CAP,
-        b=0,
-        c=-1,
-        d=1.0,
-    )
+def limitHeroesForCiv(civ_id: int, data: DatFile, hidden_resource_id: int) -> int:
+    logging.info(f'Making effect for hero unit limit for civ {data.civs[civ_id].name} - {civ_id}')
 
-    #Make dead basilius not have a dead graphic
-    dead_basilius = data.civs[civ_id].units[SPECIAL_UNIT_SPAWN_BASILIEUS_DEAD]
-    #remove basilius dead graphic
+    #copy dead basilius to give one of hiden resource at start of game.
+    dead_basilius = clone(data.civs[civ_id].units[SPECIAL_UNIT_SPAWN_BASILIEUS_DEAD], data.version)
+    dead_basilius_id = len(data.civs[civ_id].units)
+    dead_basilius.id = dead_basilius_id
+    dead_basilius.resource_storages = (
+        ResourceStorage(type=12, amount=300.0, flag=0), 
+        ResourceStorage(type=hidden_resource_id, amount=1.0, flag=1), 
+        ResourceStorage(type=-1, amount=0.0, flag=0)  
+    )
+    #remove basilius graphic so the dead unit assignment isnt funky.
     dead_basilius.standing_graphic = (-1,-1)
-    dead_basilius.dying_graphic = -1 #PETARD_DEATH_ANIMATION #TODO maybe give an explosion (petard) a flash of light or more obvious death animation
-    #create a dead basilieus at start to give one of needed resource for the unit. This one gives instant resource 501 when it dies instead of delayed
-    create_basilius_effect_command = EffectCommand(type=TYPE_SPAWN_UNIT, a=SPECIAL_UNIT_SPAWN_BASILIEUS_DEAD, b=TOWN_CENTER, c=1, d=0.0)
-    limit_unit_creatable_effect = Effect(
-        name=f'Limit Hero Amount Effect for {data.civs[civ_id].units[unit_id].name}',
-        effect_commands=[limit_unit_creatable_effect_command, create_basilius_effect_command]
-    )
+    dead_basilius.dying_graphic = -1
+    #add basilius to the civs unit list so it can be used in the effect.
+    data.civs[civ_id].units.append(dead_basilius)
+    print('dead_basilius', dead_basilius)
 
+    #create a dead basilieus at start to give one of needed resource for the unit. This one gives instant resource 501 when it dies instead of delayed
+    give_resource_at_first_tc_effect_command = EffectCommand(type=TYPE_SPAWN_UNIT, a=dead_basilius_id, b=TOWN_CENTER, c=1, d=0.0)
+    limit_unit_creatable_effect = Effect(
+        name=f'Limit Hero Amount Effect for {data.civs[civ_id].name}',
+        effect_commands=[give_resource_at_first_tc_effect_command]
+    )
     limit_unit_creatable_effect_id = len(data.effects)
     data.effects.append(limit_unit_creatable_effect)
-
-    logging.info(f'Making tech for hero unit limit for {data.civs[civ_id].units[unit_id].name} - {unit_id} for civ {data.civs[civ_id].name} - {civ_id}')
+    #add the tech with the effect to the civ to give 1 hero value.
+    logging.info(f'Making tech for hero unit limit for {data.civs[civ_id].name} - {civ_id}')
     limit_hero_unit_creatable_effect = Tech(
         required_techs=(TECH_REQUIREMENT_IMPERIAL_AGE, TYPE_TOWN_CENTER_BUILT, -1, -1, -1, -1),
         resource_costs=(
@@ -177,10 +183,11 @@ def limitCivToOneHero(civ_id: int, unit_id: int, data: DatFile):
         language_dll_help=0,
         language_dll_tech_tree=0,
         hot_key=-1,
-        name=f'Limit Hero Unit amount for {data.civs[civ_id].units[unit_id].name}',
+        name=f'Limit Hero Unit amount for civ {data.civs[civ_id].name}',
         repeatable=0,
     )
     data.techs.append(limit_hero_unit_creatable_effect)
+    return dead_basilius_id
 
 def extendTasks(unit: Unit, tasks) -> Unit:
     #set unit to use aura abilities
@@ -196,30 +203,28 @@ def giveAura(unit: Unit):
     if(unit.class_ in [CAVLARY_CLASS, WARSHIP_CLASS]):
         logging.info("giving move speed aura to unit")
         extendTasks(unit, movementSpeed)
-    elif(unit.class_ in [CAVALRY_ARCHER_CLASS]):
+    elif(unit.class_ in [CAVALRY_ARCHER_CLASS, CONQUISTADOR_CLASS]):
         logging.info("giving attack speed aura to unit")
         extendTasks(unit, attackSpeed)
     elif(unit.class_ in [INFANTRY_CLASS, ARCHER_CLASS, HAND_CANNONEER_CLASS]):
         logging.info("giving move and attack speed aura to unit")
         extendTasks(unit, attackSpeed)
         extendTasks(unit, movementSpeed)
-    elif(unit.class_ in [MONK_CLASS]):
+    elif(unit.class_ in [MONK_CLASS, HEALER_CLASS]):
         logging.info("giving healing aura to unit")
         extendTasks(unit, healing)
     else:
         logging.error(f"Unit {unit.name} not given an aura")
     return unit
 
-def makeHero(unitData: dict, civ: Civ, data: DatFile) -> int:
+def makeHero(unitId: int, civ: Civ, data: DatFile, land_basilius_unit_id: int, water_basilius_unit_id: int) -> int:
     #prevent_hp_increase(cloned_unit)
-    logging.info(f'Patching hero unit {unitData["unitId"]}')
+    logging.info(f'Patching hero unit {unitId}')
     new_unit_id = len(civ.units)
     #clone the unit
-    unit = clone(civ.units[unitData["unitId"]], data.version)
+    unit = clone(civ.units[unitId], data.version)
     #set id to be end of civ units
     unit.id = new_unit_id
-    #this gives back a resource when the unit dies that the unit costs to spawn. This limits us to one.
-    unit.dead_unit_id = SPECIAL_UNIT_SPAWN_BASILIEUS_DEAD
 
     #give the unit an aura based on its class
     unit = giveAura(unit)
@@ -231,23 +236,35 @@ def makeHero(unitData: dict, civ: Civ, data: DatFile) -> int:
         ResourceStorage(type=TYPE_TOTAL_UNITS_OWNED, amount=1, flag=1),
     )
 
-    #make unit cost resources 
-    unit.creatable.resource_costs = (
-        #this is how basilius dead works to respawn on intervals
-        ResourceCost(type=TYPE_UNIT_LIMIT_RESOURCE, amount=1, flag=1),
-        ResourceCost(type=TYPE_FOOD_STORAGE, amount=500, flag=1),
-        ResourceCost(type=TYPE_GOLD_STORAGE, amount=500, flag=1),
-    )
-
-    #make unit trainable in the castle
+    #make unit trainable in the dock if waship and limit is with water basilius
     if(unit.class_ == WARSHIP_CLASS):
         logging.info(f'chose dock for hero unit {unit.name} for civ {civ.name}')
         unit.creatable.train_location_id = TYPE_DOCK_TRAIN_LOCATION
         unit.creatable.button_id = 30
+        #this gives back a resource when the unit dies that the unit costs to spawn. This limits us to one.
+        unit.dead_unit_id = water_basilius_unit_id
+        #make unit cost resources 
+        unit.creatable.resource_costs = (
+            #lock it into a water cost
+            ResourceCost(type=WATER_BASILIUS_RESOURCE_VALUE, amount=1, flag=1),
+            ResourceCost(type=TYPE_FOOD_STORAGE, amount=500, flag=1),
+            ResourceCost(type=TYPE_GOLD_STORAGE, amount=500, flag=1),
+        )
+    #make unit trainable in the castle if other unit type and limit is with land basilius
     else:
         logging.info(f'chose castle for hero unit {unit.name} for civ {civ.name}')
         unit.creatable.train_location_id = TYPE_CASTLE_TRAIN_LOCATION
         unit.creatable.button_id = 4
+        #this gives back a resource when the unit dies that the unit costs to spawn. This limits us to one.
+        unit.dead_unit_id = land_basilius_unit_id
+        #make unit cost resources 
+        unit.creatable.resource_costs = (
+            #lock it into a water cost
+            ResourceCost(type=LAND_BASILIUS_RESOURCE_VALUE, amount=1, flag=1),
+            ResourceCost(type=TYPE_FOOD_STORAGE, amount=500, flag=1),
+            ResourceCost(type=TYPE_GOLD_STORAGE, amount=500, flag=1),
+        )
+        
     unit.creatable.hero_mode = 1
 
     #add the new unit to the civ
@@ -256,18 +273,23 @@ def makeHero(unitData: dict, civ: Civ, data: DatFile) -> int:
     return new_unit_id
 
 
+
 def mod(data: DatFile):
+    civs_missing_hero = []
     for civ_id, civ in enumerate(data.civs):
-        if civ.name not in IGNORED_CIVS and HERO_FOR_CIV[civ.name]["unitId"] is not None:
+        if civ.name in HERO_FOR_CIV and HERO_FOR_CIV[civ.name] is not None:
             logging.info(f'Creating hero for civ {civ.name}')
-            hero = HERO_FOR_CIV[civ.name]
-            new_unit_id = makeHero(hero, civ, data)
-            addUnitToCiv(civ_id, new_unit_id, data)
-            limitCivToOneHero(civ_id, new_unit_id, data)
+            land_dead_basilius_id = limitHeroesForCiv(civ_id, data, LAND_BASILIUS_RESOURCE_VALUE)
+            water_dead_basilius_id = limitHeroesForCiv(civ_id, data, WATER_BASILIUS_RESOURCE_VALUE)
+            unit_id = HERO_FOR_CIV[civ.name][0]
+            for unit_id in HERO_FOR_CIV[civ.name]:
+                hero_unit_id = makeHero(unit_id, civ, data, land_dead_basilius_id, water_dead_basilius_id)
+                addUnitToCiv(civ_id, hero_unit_id, data)
             logging.info(f'Creating hero for civ other civs')
-#            for rest_of_civs_civ_id, rest_of_civs_civ in enumerate(data.civs):
-#                if rest_of_civs_civ_id != civ_id:
-#                    #make the hero for the other civs
-#                    makeHero(hero, rest_of_civs_civ, data)
         else:
-            logging.error(f'Failed to find hero for civ {civ.name}')
+            if(civ.name not in CIVS_WITH_HEROES_ALREADY):
+                # If the civ is not in the list, log an error message
+                civs_missing_hero.append(civ.name)
+    
+    for civ in civs_missing_hero:
+        logging.error(f'No hero for civ {civ}')
